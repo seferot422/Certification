4e9513d1e5e3a4e7073b1a54ae22b158ac47a042
Sergey Komarov <demon150196@bk.ru>
2023-12-11 20:26:05 +0300

    decide Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,13 +2,12 @@
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Пожалуйста, Введите значение N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-void che(
-int m, int n)
- {
-    if (m > n) return;
-    if (m % 2 == 0) {
-        Console.Write($"{m}, ");
-    }
-    che(m+1, n);
+
+int akkerman(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return akkerman(m - 1, 1);
+else return akkerman(m - 1, akkerman(m, n - 1));
 }
-che(m, n);
+
+Console.Write($"Функция Аккермана равна {akkerman(m, n)} ");
