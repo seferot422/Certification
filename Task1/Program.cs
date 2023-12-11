34d92c7ef65023ac9a556ce4ab409c96573178b2
Sergey Komarov <demon150196@bk.ru>
2023-12-11 20:18:54 +0300

    decide Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Пожалуйста, Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Пожалуйста, Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void che( int m, int n)
+    {
+        if (m>n) return;
+        if (m % 2 == 0) {
+            Console.Write($"{m}, ");
+        }
+        che(m+1, n);
+    }
+che(m, n);
