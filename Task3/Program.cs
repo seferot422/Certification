4dd391c5b5b2cf293528635f564f06c6f13fcf91
Sergey Komarov <demon150196@bk.ru>
2023-12-11 20:53:32 +0300

    decide Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] a = { 1, 2, 5, 0, 10, 4 };
+        Print(a, 0);
+    }
+
+    static void Print(int[] array, int index)
+    {
+        try
+        {
+            int element = array[index];
+            Print(array, index + 1);
+            Console.WriteLine(element);
+        }
+        catch (IndexOutOfRangeException ignored) { }
+    }
+}
